445befc04751dae22beb73628e78460fd8912fec
Katie Stevens Andersen <KAstev@gmail.com>
2022-07-15 15:08:19 -0600

    Get rid of set_option code

--- tools/kese_raw_data_fetch.py
+++ tools/kese_raw_data_fetch.py
@@ -7,14 +7,6 @@ import tools.kese_helpers as h
 from kauffman.data import bed, pep
 from kauffman.tools import file_to_s3
 
-pd.set_option('max_columns', 1000)
-pd.set_option('max_info_columns', 1000)
-pd.set_option('expand_frame_repr', False)
-pd.set_option('display.max_rows', 30000)
-pd.set_option('max_colwidth', 4000)
-pd.set_option('display.float_format', lambda x: '%.3f' % x)
-pd.set_option('chained_assignment',None)
-
 
 def _cps():
     df_us = pd.DataFrame()
